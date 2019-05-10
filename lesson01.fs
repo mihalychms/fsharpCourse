6aec3bdb9c09813e9783a521de26a810cd7061b9
mihalychms <mihalychms@gmail.com>
2019-05-10 19:00:41 +0300

    Update lesson01.fs

--- lesson01.fs
+++ lesson01.fs
@@ -1,5 +1,3 @@
-let g n = n + 5 // int
-
-let gg = fun n -> n + 5
-
-let h (x,y) = System.Math.Sqrt(System.Math.Pow(x, 2.0) + System.Math.Pow(y, 2.0))
+let days_in_month = function
+| n when n < 1 || n > 12  -> 0
+| n -> System.DateTime.DaysInMonth(System.DateTime.Now.Year, n)
