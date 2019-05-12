4873f00a523fb676db3e4c040ff359a83d417071
mihalychms <mihalychms@gmail.com>
2019-05-12 14:29:14 +0300

    Update lesson01.fs

--- lesson01.fs
+++ lesson01.fs
@@ -1,3 +1,14 @@
-let days_in_month = function
-| n when n < 1 || n > 12  -> 0
-| n -> System.DateTime.DaysInMonth(System.DateTime.Now.Year, n)
+// 7.1.1
+let rec fibo = function 
+| n when n < 2 -> n
+| n -> (fibo(n-1) + fibo(n-2))
+
+// 7.1.2
+let rec sum = function
+| n when n < 2 -> 1
+| n -> (sum(n-1) + n)
+
+// 7.1.3
+let rec sum2 = function 
+ | (m,0) -> m
+ | (m,n) -> m + sum2(m, n-1)
