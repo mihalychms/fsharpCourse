b1bfa29e1ef66030379929b657e60d8445835954
mihalychms <mihalychms@gmail.com>
2019-05-14 22:08:49 +0300

    Create code.fs

---
+++ code.fs
@@ -0,0 +1,15 @@
+// 7.1.1
+let rec fibo = function 
+| n when n = 0 -> 0
+| n when n = 1 -> 1
+| n -> (fibo(n-1) + fibo(n-2))
+
+// 7.1.2
+let rec sum = function
+| n when n < 2 -> 1
+| n -> (sum(n-1) + n)
+
+// 7.1.3
+let rec sum2 = function 
+ | (m,0) -> m
+ | (m,n) -> m + sum2(m, n-1)
